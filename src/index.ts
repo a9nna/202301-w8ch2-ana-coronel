99179851fac94af1911349911c4acc2a4c24893f
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-28 22:42:49 +0100

    Create getMeanAverageSalaries

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import clients from "./clients.js";
 import getClientsDniWithLandline from "./utils/getClientsWithLandline/getClientsDniWithLandline.js";
 import getClientsWithStablishAverageSalary from "./utils/getClientsWithStablishAverageSalary/getClientsWithStablishAverageSalary.js";
+import getMeanAverageSalaries from "./utils/getMeanAverageSalaries/getMeanAverageSalaries.js";
+
+const minimumSalary = 5000;
 
 getClientsDniWithLandline(clients);
-getClientsWithStablishAverageSalary(clients, 5000);
+
+const clientsWithStablishAverageSalary = getClientsWithStablishAverageSalary(
+  clients,
+  minimumSalary
+);
+
+const meanAvarageSalaries = getMeanAverageSalaries(
+  clientsWithStablishAverageSalary
+);
+
+console.log(
+  `El saldo medio promedio de los clientes con nómina y con saldo medio menor a ${minimumSalary}€ es: ${meanAvarageSalaries}€`
+);
